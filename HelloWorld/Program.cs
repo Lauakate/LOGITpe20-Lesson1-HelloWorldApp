80d1697832993b7fc318a75e75221df9241f2b45
Opilane TTHK <opilane@tthk.ee>
2020-09-01 13:39:20 +0300

    my first app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //this is a comment
+            // Console.WriteLine("Hello Rix!");
+
+            /* 1. 
+             * 2.
+             * 3.
+             */
+
+            //Above is a list
+            Console.WriteLine("What is your name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello, " + Name);
+
+        }   
+    }
+}
